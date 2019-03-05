7ca83d22b3cf9826882219a8dbcdb51d3172ae5a
Marius Erlandsen <marius.s.erlandsen@gmail.com>
2019-03-05 14:29:06 +0100

    legger til en til

--- httpHandler/main_test.go
+++ httpHandler/main_test.go
@@ -16,11 +16,11 @@ func router() *http.ServeMux {
 // inspirert fra nett. Er den riktig? Må/burde vel kalle response.Result()?
 // noe voldsomt med mux og kanskje?
 func TestIndex(t *testing.T) {
-	request, _ := http.NewRequest("GET", "/", nil)
-	response := httptest.NewRecorder()
-	router().ServeHTTP(response, request)
-	if response.Body.String() != "Hello, World" {
-		t.Errorf("\nexpected: Hello, World\ngot: %s", response.Body.String())
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	router().ServeHTTP(w, req)
+	if w.Body.String() != "Hello, World" {
+		t.Errorf("\nexpected: Hello, World\ngot: %s", w.Body.String())
 	}
 }
 
@@ -33,5 +33,17 @@ func TestIndex2(t *testing.T) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	if string(body) != "Hello, World" {
 		t.Errorf("\nexpected: Hello, World\ngot: %s", string(body))
+	}
+}
+
+// funker like herlig uten w.Result() forsåvidt...
+func TestIndex3(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	index(w, req)
+	// resp := w.Result()
+	//body, _ := ioutil.ReadAll(resp.Body)
+	if string(w.Body.String()) != "Hello, World" {
+		t.Errorf("\nexpected: Hello, World\ngot: %s", w.Body.String())
 	}
 }
