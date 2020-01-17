124f6d39684da39c5568768244ccadf7c24ca94d
Marius Erlandsen <marius.erlandsen@nav.no>
2020-01-17 08:15:37 +0100

    adding one way to mock the execution of an http request

---
+++ httpRequestMock/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type roundtripper struct{}
+
+func (r roundtripper) RoundTrip(*http.Request) (*http.Response, error) {
+	return &http.Response{
+		StatusCode: 555,
+		Body:       ioutil.NopCloser(bytes.NewBufferString(`OK`)),
+		Header:     make(http.Header),
+	}, nil
+}
+
+func newHTTPClient() *http.Client {
+	return &http.Client{
+		Transport: roundtripper{},
+	}
+}
+
+func main() {
+	req, err := http.NewRequest("GET", "http://goosdgsdfghsdfhsdfhgle.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cli := newHTTPClient()
+	response, err := cli.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(response.StatusCode)
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+}
